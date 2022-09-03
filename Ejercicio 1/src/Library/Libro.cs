1a0e545227a4cd5967b6ec2e2c2662c2165a4f7a
atoya200 <agustintoya2004@gmail.com>
2022-09-03 11:40:10 -0300

    Comentarios que hice en clase

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -18,11 +18,29 @@ namespace SRP
             this.Code = code;
         }
 
+    /* 
+        Si cambia la forma en la que se almacenan los libros, hay que registrar donde estan ahora, 
+        en que biblioteca estan. Si estan en físico o digital. Entonces hay que cambiar. 
+     */
         public void ShelveBook(String sector, String shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
 
+    /* 
+        Si se quiere guardar en otro momento o dejar una orden para guardar, es decir
+        dejar anotado que se quiere guardar o se debe guardar. No hay un seguimiento de la 
+        acción en sí. Se guarda y listo, pero en una biblioteca grande como la es la de la UCU, puede
+        digitarse el guardado del libro el día de hoy pero hacerse realmente en 3 días. Entonces, si se quiere
+        dejar esa orden debería haber una clase que lo haga. 
+
+        Si se quisiera saber que funcionario de la biblioteca fue el que guardo el libro, habría que crear 
+        una clase exclusiva para almacenar, teniendo en cuenta el nombre o código de funcionario.
+   */
     }
+
+
+   // public class ShelveBook:
+
 }
